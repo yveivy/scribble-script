7702dc59ec8a6aaa37be1e2c0409027077dcdd45
Valais Girl <Yevette@Hal-71.local>
2023-06-06 14:55:18 -0500

    moved everyting out of develop folder and deleted

---
+++ client/src/js/database.js
@@ -0,0 +1,50 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+ 
+export const putDb = async (content) => {
+
+const jateDb = await openDB ('jate', 1);
+
+const tx = contactDb.transaction('jate', 'readwrite');
+
+const store = tx.objectStore ('jate');
+
+const request =store.add({ id: 1, value: content });
+
+const result = await request;
+console.log('data saved to the database', result);
+
+};
+
+
+export const getDb = async () => {
+
+const jateDb = await openDB ('jate', 1);
+
+const tx = contactDb.transaction ('jate', 'readonly');
+
+const store = tx.objectStore ('jate');
+
+const request = store.get(1);
+
+const result = await request;
+
+console.log ('result.value', result);
+
+};
+
+
+
+initdb();
